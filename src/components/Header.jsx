2a5b52c3dc0bdd4aaf1c81ba4b32cb592cbd5e94
rahul_004 <rahulbky2004@gmail.com>
2023-12-26 03:03:16 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BoltIcon from '@mui/icons-material/Bolt';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+      <BoltIcon style={{color:'#0cdbe1'}}></BoltIcon>
+      QuikNotes</h1>
+    </header>
+  );
+}
+
+export default Header;
